3d986468ae358222f684f034c722112fd624c2d0
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2023-09-15 22:38:58 +0200

    Added images etc
    nu weekend, maandag verder joe

--- src/routes/prisoners/+page.svelte
+++ src/routes/prisoners/+page.svelte
@@ -1,69 +1,101 @@
 <script>
-    import * as prismic from '@prismicio/client'
-    import { SliceZone } from '@prismicio/svelte'
-    import { components } from '$lib/slices'
+  import * as prismic from "@prismicio/client";
+  import { SliceZone } from "@prismicio/svelte";
+  import { components } from "$lib/slices";
 
-    export let data
+  export let data;
 
-    //MOVE PRISONERS FUNCTION//
-    let prisoners = data.list
-    console.log(data.list)
-    console.log(prisoners.length)
+  //MOVE PRISONERS FUNCTION//
+  let prisoners = data.list;
+  console.log(data.list);
+  console.log(prisoners.length);
 
-    let index = 0
-    
-    const nextPrisoner = () =>{
-        index = (index + 1) % prisoners.length
-    }
+  let index = 0;
 
-    const previousPrisoner = () => {
+  const nextPrisoner = () => {
+    index = (index + 1) % prisoners.length;
+  };
+
+  const previousPrisoner = () => {
     if (index != 0) {
       index = (index - 1) % prisoners.length;
     } else {
       index = prisoners.length - 1;
     }
-    };
+  };
 </script>
 
 <section>
-    <button on:click={previousPrisoner}>
-        <img src="/arrow-left.svg" alt="button to move to previous prisoner">
-    </button>
-    <div class="prisoner">
-        <h2>
-            {@html prismic.asHTML(prisoners[index].data.name)}
-            <img class="creator-image" src="{prisoners[index].data.image.url}" alt="prisoner"/>
-        </h2>
-    </div>
-    <button on:click={nextPrisoner}>
-        <img src="/arrow-right.svg" alt="button to move to next prisoner">
-    </button>
+  <h2>
+    {@html prismic.asHTML(prisoners[index].data.name)}
+  </h2>
+
+  <button on:click={previousPrisoner}>
+    <img src="/arrow-left.svg" alt="button to move to previous prisoner" />
+  </button>
+
+  <div class="prisoner">
+    <img
+      class="person-head"
+      src={prisoners[index].data.image.url}
+      alt="prisoner"
+    />
+    <img
+      class="person-body"
+      src="./person-prison-body.png"
+      alt="prisoner-body"
+    />
+  </div>
+
+  <button on:click={nextPrisoner}>
+    <img src="/arrow-right.svg" alt="button to move to next prisoner" />
+  </button>
 </section>
 
 <style>
-    section{
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 4rem;
-        background-color: black;
-        background: url("./prisoncell.png");
-        background-size: cover;
-        background-position: center;
-    }
+  section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background: url("./prisoncell.png");
+    background-size: cover;
+    background-position: center;
+  }
 
-    h2{
-        width: 100px;
-        text-align: center;
-    }
+  h2 {
+    position: absolute;
+    text-align: center;
+    width: 100%;
+  }
 
-    button{
-        text-decoration: none;
-        background-color: transparent;
-        border: none;
-        outline: none;
-    }
+  button {
+    text-decoration: none;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    z-index: 10;
+  }
 
+  .prisoner {
+    position: relative;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40vw;
+    height: 50vh;
+  }
+
+  .person-head {
+    position: absolute;
+    top: 20px;
+    width: 150px;
+  }
+
+  .person-body {
+    width: 200px;
+  }
 </style>
